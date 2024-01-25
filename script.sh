71a3d34cc6492050ac678f6c808b626edb5b9d4f
mars <rabbid15@hotmail.com>
2024-01-25 05:31:30 +0000

    Modify the scriptw

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 sudo apt-get install zsh powerline fonts-powerline tree -y
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
@@ -10,3 +10,15 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 cp zshrc_linux ~/.zshrc
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+
+sudo apt-get install npm
+sudo npm install tree-sitter-cli
+
